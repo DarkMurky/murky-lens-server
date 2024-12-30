63342a239ff5be85e483667ed78aed2bd1c3bf1a
DarkMurky <karolisvaitiekunas@protonmail.com>
2024-12-31 00:42:40 +0200

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express, { type Express } from "express";
+import cors from "cors";
 
 import globalErrorHandler from "@utils/errorHandling";
 
@@ -9,6 +10,8 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
