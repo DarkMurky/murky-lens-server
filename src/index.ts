75af3b82fc76e2da38b811617660c829d95d2eaf
DarkMurky <karolisvaitiekunas@protonmail.com>
2025-01-02 01:34:29 +0200

    feat: allow cors from eveywhere

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express, { type Express } from "express";
 import cors from "cors";
 
 import globalErrorHandler from "@utils/errorHandling";
-
 import routes from "@routes/index";
 
 dotenv.config();
@@ -11,14 +10,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+	}),
+);
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-
 app.use(routes);
-
 app.use(globalErrorHandler);
 
 app.listen(port, () => {
