1d7184d285a9fc3827292e98a14b2b8eaed5a7d9
DarkMurky <166627327+DarkMurky@users.noreply.github.com>
2025-02-17 00:07:05 +0200

    Revert "feat: JWT auth"
    Revert "feat: JWT auth"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
 
-import authRouter from "@routes/auth.routes";
 import lensRouter from "@routes/lens.routes";
-import userRouter from "@routes/user.routes";
 
 const router = Router();
 
 router.use(lensRouter);
-router.use(userRouter);
-router.use(authRouter);
 
 export default router;
